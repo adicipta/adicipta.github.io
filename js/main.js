e9442b9d070e7e74d751d47a64a1aadf3790cc9a
Adi Cipta Pratama <only.adicipta@gmail.com>
2026-05-12 09:21:47 +0700

    chore: fix carousel

--- js/main.js
+++ js/main.js
@@ -89,30 +89,37 @@
 	};
 
 	onePageClick();
-	
 
 	var carousel = function() {
 		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:false,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:1
-	      },
-	      1000:{
-	        items:1
-	      }
-	    }
+		loop:true,
+		autoplay: true,
+		margin:0,
+		animateOut: 'fadeOut',
+		animateIn: 'fadeIn',
+		nav:false,
+		autoplayHoverPause: false,
+		items: 1,
+
+		touchDrag: false,
+		mouseDrag: false,
+
+		navText : [
+		"<span class='ion-md-arrow-back'></span>",
+		"<span class='ion-chevron-right'></span>"
+		],
+
+		responsive:{
+		0:{
+			items:1
+		},
+		600:{
+			items:1
+		},
+		1000:{
+			items:1
+		}
+		}
 		});
 	};
 	carousel();
